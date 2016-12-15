edcdf1463fa7bdf01a01c51d1a27e12c00bb4134
Bárta Michal - Allrisk <barta.michal@allrisk.cz>
2016-12-15 12:35:27 +0100

    grunt a admin

---
+++ Gruntfile.js
@@ -0,0 +1,24 @@
+﻿module.exports = function(grunt) {
+    grunt.initConfig({
+        less: {
+            development: {
+                options: {
+                    paths: ["html/less"],
+                    yuicompress: true
+                },
+                files: {
+                    "html/css/style.css": "html/less/style.less",
+                    "html/css/admin/style.css": "html/less/admin/style.less"
+                }
+            }
+        },
+        watch: {
+            files: "html/less/admin/*",
+            tasks: ["less"]
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', 'watch'); // registrace defaultní úlohy
+};
